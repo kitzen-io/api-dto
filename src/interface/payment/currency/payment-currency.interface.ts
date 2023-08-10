0095e6f1be0b5a06105dbe70f62fd73e48222fe0
Anthony <anton@prominence.tech>
2023-08-10 21:54:35 +0200

    currency response;

--- src/interface/payment/currency/payment-currency.interface.ts
+++ src/interface/payment/currency/payment-currency.interface.ts
@@ -1,10 +1,6 @@
-import CurrencyTypeEnum from '../../../enum/currency/currency-type.enum';
-
 export default interface IPaymentCurrency {
   id: number;
   identifier: string;
-  network: string;
   rate: number;
-  type: CurrencyTypeEnum;
 }
 
