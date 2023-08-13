c6bcf611605bb40a98c8199da64fb21707653550
Anthony <anton@prominence.tech>
2023-08-13 17:08:48 +0200

    added value to supported crypto currencies;

--- src/constant/common/crypto-currency.ts
+++ src/constant/common/crypto-currency.ts
@@ -2,9 +2,10 @@ import { BlockchainNetworkEnum } from "../../enum";
 
 const CRYPTO_CURRENCY = [
   {
+    label: 'BTC',
+    value: `BTC_${BlockchainNetworkEnum.BTC}`,
     network: BlockchainNetworkEnum.BTC,
     identifier: 'coin',
-    symbol: 'BTC',
   },
 ];
 
