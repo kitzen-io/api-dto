d0206d691e0fc660fa7c5afa92c6fe75ca875fde
neese <temanharosh@gmail.com>
2023-08-14 17:45:15 +0300

    fix: missed useful enum

--- src/interface/messages/index.ts
+++ src/interface/messages/index.ts
@@ -1,9 +1,9 @@
-import { BackgroundContentMessageTypesEnum, ExtensionNotificationActionEnum, ExtensionsPostMessageTypesEnum } from '../../enum';
+import { BackgroundContentMessageTypesEnum, BackgroundMessageTypeEnum, ExtensionNotificationActionEnum, ExtensionsPostMessageTypesEnum } from '../../enum';
 
 export * from './extension';
 export * from './common';
 
 export interface IMessage<T> {
-  messageType: BackgroundContentMessageTypesEnum | ExtensionNotificationActionEnum | ExtensionsPostMessageTypesEnum;
+  messageType: BackgroundMessageTypeEnum | BackgroundContentMessageTypesEnum | ExtensionNotificationActionEnum | ExtensionsPostMessageTypesEnum;
   data?: T;
 }
