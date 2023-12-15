b790a409ea3c0d4441de8b93ce1f42bd5e08b7e9
neese <temanharosh@gmail.com>
2023-12-15 14:37:40 +0200

    fix: add eth and usdc to cryptocurrencies p2p

--- src/constant/common/crypto-currency.ts
+++ src/constant/common/crypto-currency.ts
@@ -1,4 +1,4 @@
-import {BlockchainNetworkEnum} from "../../enum";
+import { BlockchainNetworkEnum } from '../../enum';
 
 const CRYPTO_CURRENCY = [
   {
@@ -6,6 +6,18 @@ const CRYPTO_CURRENCY = [
     value: `${BlockchainNetworkEnum.BTC}_coin`,
     network: BlockchainNetworkEnum.BTC,
     identifier: 'coin',
+  },
+  {
+    label: 'ETH',
+    value: `${BlockchainNetworkEnum.ETH}_coin`,
+    network: BlockchainNetworkEnum.ETH,
+    identifier: 'coin',
+  },
+  {
+    label: 'USDC',
+    value: `${BlockchainNetworkEnum.ETH}_0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48`,
+    network: BlockchainNetworkEnum.ETH,
+    identifier: '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48',
   },
 ];
 
