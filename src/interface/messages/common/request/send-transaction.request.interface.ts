127afa871752e8c11fcd597f31e26db52354ee5b
neese <temanharosh@gmail.com>
2023-12-18 17:06:45 +0200

    fix: add buyer and paymentWindowInSeconds fields to send request

--- src/interface/messages/common/request/send-transaction.request.interface.ts
+++ src/interface/messages/common/request/send-transaction.request.interface.ts
@@ -3,8 +3,10 @@ import { BlockchainNetworkEnum } from '../../../../enum';
 export interface ISendTransactionRequestCommonMessageData {
   to: string;
   amount: string;
+  buyer?: string;
   network: BlockchainNetworkEnum;
   currency?: string;
   identifier: string;
   tradeId: string;
+  paymentWindowInSeconds?: number;
 }
