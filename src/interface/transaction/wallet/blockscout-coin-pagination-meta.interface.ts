b9e96a1656415ef0cb6d9c9177bba5b21ae2699a
Ruslan Panasiuk <ruslan.panasiuk@prominence.tech>
2023-12-19 16:10:52 +0200

    Fixing eth pagination

--- src/interface/transaction/wallet/blockscout-coin-pagination-meta.interface.ts
+++ src/interface/transaction/wallet/blockscout-coin-pagination-meta.interface.ts
@@ -2,4 +2,8 @@ export default interface BlockscoutCoinPaginationMeta {
   blockNumber: number;
   index: number;
   itemsCount: number;
+  hash: string;
+  fee: string;
+  value: string;
+  insertedAt: Date;
 }
