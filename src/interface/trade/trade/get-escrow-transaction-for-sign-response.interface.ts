796c93a574568a1bc228327c582a2d87568fdbd8
neese <temanharosh@gmail.com>
2023-12-26 18:30:23 +0200

    fix: add eth sign escrow transaction

--- src/interface/trade/trade/get-escrow-transaction-for-sign-response.interface.ts
+++ src/interface/trade/trade/get-escrow-transaction-for-sign-response.interface.ts
@@ -1,4 +1,12 @@
-export default interface IGetEscrowTransactionForSignResponse {
+type IGetEscrowTransactionForSignResponse = {
   psbtBase64: string;
   derivationPath: string;
-}
+} | {
+  tradeId: string;
+  makerAddress: string;
+  takerAddress: string;
+  amount: bigint;
+  fee: number; // half platform fee
+};
+
+export default IGetEscrowTransactionForSignResponse;
