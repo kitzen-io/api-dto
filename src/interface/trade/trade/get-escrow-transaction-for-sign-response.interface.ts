30c1104c195119e613d4024f1657216e329efe24
neese <temanharosh@gmail.com>
2023-12-26 18:31:21 +0200

    Revert "fix: add eth sign escrow transaction"
    This reverts commit 796c93a574568a1bc228327c582a2d87568fdbd8.

--- src/interface/trade/trade/get-escrow-transaction-for-sign-response.interface.ts
+++ src/interface/trade/trade/get-escrow-transaction-for-sign-response.interface.ts
@@ -1,12 +1,4 @@
-type IGetEscrowTransactionForSignResponse = {
+export default interface IGetEscrowTransactionForSignResponse {
   psbtBase64: string;
   derivationPath: string;
-} | {
-  tradeId: string;
-  makerAddress: string;
-  takerAddress: string;
-  amount: bigint;
-  fee: number; // half platform fee
-};
-
-export default IGetEscrowTransactionForSignResponse;
+}
