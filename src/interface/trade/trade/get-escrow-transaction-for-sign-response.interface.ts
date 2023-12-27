62a36ed43af747621ebd4173e3ef99c0d14316eb
neese <temanharosh@gmail.com>
2023-12-27 12:00:17 +0200

    fix: remove wrong implementation

--- src/interface/trade/trade/get-escrow-transaction-for-sign-response.interface.ts
+++ src/interface/trade/trade/get-escrow-transaction-for-sign-response.interface.ts
@@ -1,12 +1,6 @@
 type IGetEscrowTransactionForSignResponse = {
   psbtBase64: string;
   derivationPath: string;
-} | {
-  tradeId: string;
-  makerAddress: string;
-  takerAddress: string;
-  amount: bigint;
-  fee: number; // half platform fee
 };
 
 export default IGetEscrowTransactionForSignResponse;
